c02cd7becc5563a37c47510c5181b923bf659e7f
gerardo montini <germontini@hotmail.com>
2023-10-31 18:59:12 -0300

    pre-entrega

--- js/app.js
+++ js/app.js
@@ -1,1 +1,21 @@
-console.log ("conectado");
+//Crear un algoritmo con un condicional.
+
+const numero = parseFloat(prompt("Ingresa un número:"));
+
+if (numero > 0) {
+    console.log("El número es positivo");
+} else if (numero === 0) {
+    console.log("El número es igual a cero");
+} else {
+    console.log("El número es negativo");
+}
+
+//Crear un algoritmo utilizando un ciclo.
+
+let contador = 1;
+
+while (contador <= 10) {
+    console.log(contador);
+
+    contador++;
+}
